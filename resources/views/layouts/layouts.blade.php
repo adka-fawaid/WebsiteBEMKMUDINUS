c7a46b8129355335e08a0effdb7abc2fec916c0c
Wahyu NurS <whyofficial25032004@gmail.com>
2026-01-20 13:37:09 +0700

    fix: restore commit old

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -7,26 +7,26 @@
 
     {{-- Meta untuk tampil di Whatsapp --}}
     @if (Request::segment(1) == '')
-    <meta property="og:title" content="Website BEM KM UDINUS" />
-    <meta name="description" content="Badan Eksekutif Mahasiswa Keluarga Mahasiswa Universitas Dian Nuswantoro" />
-    <meta property="og:url" content="http://.com" />
-    <meta property="og:description" content="BEM KM UDINUS" />
-    <meta property="og:image" content="{{ asset('assets/icons/Logo BEM KM UDINUS.png') }}" />
-    <meta property="og:type" content="article" />
-    <title>Website BEM KM UDINUS</title>
+        <meta property="og:title" content="Website BEM KM UDINUS" />
+        <meta name="description" content="Badan Eksekutif Mahasiswa Keluarga Mahasiswa Universitas Dian Nuswantoro" />
+        <meta property="og:url" content="http://.com" />
+        <meta property="og:description" content="BEM KM UDINUS" />
+        <meta property="og:image" content="{{ asset('assets/icons/Logo BEM KM UDINUS.png') }}" />
+        <meta property="og:type" content="article" />
+        <title>Website BEM KM UDINUS</title>
     @elseif (Request::segment(1) == 'detail')
-    <meta property="og:title" content="{{ $artikel->judul }}" />
-    <meta name="description" content="{{ $artikel->judul }}" />
-    <meta property="og:url" content="http://.com/detail/{{ $artikel->slug }}" />
-    <meta property="og:description" content="{{ $artikel->judul }}" />
-    @if ($artikel->image)
-    <meta property="og:image" content="{{ asset('storage/artikel/' . $artikel->image) }}" />
-    @else
-    <meta property="og:image" content="{{ asset('assets/icons/Logo BEM KM UDINUS.png') }}" />
-    @endif
-    <meta property="og:type" content="article" />
+        <meta property="og:title" content="{{ $artikel->judul }}" />
+        <meta name="description" content="{{ $artikel->judul }}" />
+        <meta property="og:url" content="http://.com/detail/{{ $artikel->slug }}" />
+        <meta property="og:description" content="{{ $artikel->judul }}" />
+        @if ($artikel->image)
+            <meta property="og:image" content="{{ asset('storage/artikel/' . $artikel->image) }}" />
+        @else
+            <meta property="og:image" content="{{ asset('assets/icons/Logo BEM KM UDINUS.png') }}" />
+        @endif
+        <meta property="og:type" content="article" />
 
-    <title>Website BEM KM UDINUS | {{ $artikel->title }}</title>
+        <title>Website BEM KM UDINUS | {{ $artikel->title }}</title>
     @endif
 
     {{-- Meta untuk tampil di Whatsapp --}}
@@ -38,7 +38,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
@@ -167,7 +167,8 @@
                             <p><i class="fas fa-home me-3"></i>Gedung F UDINUS <a
                                     href="https://maps.app.goo.gl/nd2qTKHGDPYRi7tg8">(MAP)</a></p>
                             <p>
-                                <a href="mailto:bemkm@orma.dinus.ac.id"><i class="fas fa-envelope me-3"></i>bemkm@orma.dinus.ac.id</a>
+                                <a href="mailto:bemkm@orma.dinus.ac.id"><i
+                                        class="fas fa-envelope me-3"></i>bemkm@orma.dinus.ac.id</a>
                             </p>
                             <p><i class="fas fa-phone me-3"></i>0895710034499 (Nata)</p>
                             <p><i class="fas fa-print me-3"></i>Partnership <a
@@ -188,27 +189,7 @@
                     <!-- Grid container -->
                     <div class="container p-4 pb-0">
                         <!-- Section: CTA -->
-                        <section class="">
-                            <p class="d-flex justify-content-center align-items-center">
-                                @auth
-                            <form action="/logout" method="POST">
-                                @csrf
-                                <span class="me-3">Saya Bukan Admin BEM KM UDINUS</span>
-                                <button data-mdb-ripple-init type="submit" class="btn btn-outline-light btn-rounded">
-                                    Logout
-                                </button>
-                            </form>
-                            @else
-                            <form action="/logout" method="POST">
-                                @csrf
-                                <span class="me-3">Dashboard Admin BEM KM UDINUS</span>
-                                <button data-mdb-ripple-init type="submit" class="btn btn-outline-light btn-rounded">
-                                    Login
-                                </button>
-                            </form>
-                            @endauth
-                            </p>
-                        </section>
+
                         <!-- Section: CTA -->
                     </div>
                     <!-- Grid container -->
@@ -231,8 +212,8 @@
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -241,21 +222,19 @@
 
     <script src="{{ asset('assets/js/magnific.js') }}"></script>
 
-    <!-- {{-- JQUERY --}} 
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> -->
+    <!-- {{-- JQUERY --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+        crossorigin="anonymous"></script> -->
 
     <!-- Summernote JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs5.min.js"></script>
 
-    <script type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js"></script>
 
     <script type="text/javascript" src="https://app.diagrams.net/js/viewer-static.min.js"></script>
 
     <script type="text/javascript">
-
-        $(document).ready(function () {
+        $(document).ready(function() {
             $('#summernote').summernote({
                 height: 200,
             });
@@ -272,7 +251,6 @@
                 // navbar.classList.add("navbar-dark");
             }
         };
-
     </script>
 
     <script>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,5 @@
 <!-- Navbar -->
-<nav
-     class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0 bg-white shadow color-black">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0 bg-white shadow color-black">
 
     <div class="container">
 
@@ -22,23 +21,30 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar-nav ml-auto">
-                
+
             </ul>
 
             <!-- Tambahkan ml-auto di sini -->
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
-                    <a class="nav-link active dropdown-toggle w3-button w3-mobile" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Beranda </a>
+                    <a class="nav-link active dropdown-toggle w3-button w3-mobile" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Beranda </a>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li class="nav-item"><a href="/kabinet" class="nav-link w3-button w3-mobile dropdown-item">Kabinet Aksa Sinergi</a></li>
-                        <li class="nav-item"><a href="/forum" class="nav-link w3-button w3-mobile dropdown-item">Forum dan Kajian</a></li>
-                        <li class="nav-item"><a href="/proker" class="nav-link w3-button w3-mobile dropdown-item">Program Kerja</a></li>
-                        <li class="nav-item"><a href="/ormawa" class="nav-link w3-button w3-mobile dropdown-item">Ormawa UDINUS</a></li>
-                        <li class="nav-item"><a href="/ukm" class="nav-link w3-button w3-mobile dropdown-item">UKM UDINUS</a></li>
+                        <li class="nav-item"><a href="/kabinet"
+                                class="nav-link w3-button w3-mobile dropdown-item">Kabinet Aksa Sinergi</a></li>
+                        <li class="nav-item"><a href="/forum" class="nav-link w3-button w3-mobile dropdown-item">Forum
+                                dan Kajian</a></li>
+                        <li class="nav-item"><a href="/proker"
+                                class="nav-link w3-button w3-mobile dropdown-item">Program Kerja</a></li>
+                        <li class="nav-item"><a href="/ormawa" class="nav-link w3-button w3-mobile dropdown-item">Ormawa
+                                UDINUS</a></li>
+                        <li class="nav-item"><a href="/ukm" class="nav-link w3-button w3-mobile dropdown-item">UKM
+                                UDINUS</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link w3-button w3-mobile" href="https://docs.google.com/forms/d/e/1FAIpQLSeDNiL7fvSjR2IHC3DDOpwghKzlBshTjA0eZ9YRZGGUSWX3Bg/viewform">Partnership</a>
+                    <a class="nav-link w3-button w3-mobile"
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSeDNiL7fvSjR2IHC3DDOpwghKzlBshTjA0eZ9YRZGGUSWX3Bg/viewform">Partnership</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link w3-button w3-mobile" href="/berita">Content</a>
@@ -47,7 +53,8 @@
                     <a class="nav-link w3-button w3-mobile" href="/foto">Kegiatan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link w3-button w3-mobile" href="https://sites.google.com/mhs.dinus.ac.id/kalenderkm">Kalender</a>
+                    <a class="nav-link w3-button w3-mobile"
+                        href="https://sites.google.com/mhs.dinus.ac.id/kalenderkm">Kalender</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link w3-button w3-mobile" href="https://wa.me/895710034499">Kontak</a>
